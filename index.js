812653f9ee771696a2314cb833b85d3c90507dee
Yuyi <yuyi365@gmail.com>
2021-09-19 15:35:18 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+main.remove(main);
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+document.body.appendChild(newHeader);
+newHeader.innerHTML = 'YOUR-NAME is the champion';
